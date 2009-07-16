0046a1f9030ced3776d3d4fbb57b46cb97d0aa3b
Eduardo Habkost <ehabkost@raisama.net>
2009-07-16 17:54:05 -0300

    Accept (almost) any character in URLs
    Signed-off-by: Eduardo Habkost <ehabkost@raisama.net>

--- bot_sql.py
+++ bot_sql.py
@@ -169,7 +169,7 @@ while True:
 	regexs  = re.compile('PRIVMSG.*[: ]\@slackers', re.UNICODE)
 	regexks = re.compile('PRIVMSG.*[: ]\@karmas', re.UNICODE)
 	regexslack  = re.compile(':([a-zA-Z0-9\_]+)!.* PRIVMSG.* :(.*)$', re.UNICODE)
-	pattern_url   = re.compile(':([a-zA-Z0-9\_]+)!.* PRIVMSG .*(http://[áéíóúÁÉÍÓÚÀàa-zA-Z0-9_?=./,\-\+\'~]+)', re.UNICODE)
+	pattern_url   = re.compile(':([a-zA-Z0-9\_]+)!.* PRIVMSG .*(http://[^ \t>\n\r]+)', re.UNICODE)
 	
 	resultp  = regexp.search(buffer)
 	resultm  = regexm.search(buffer)
