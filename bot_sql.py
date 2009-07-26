e997233a9f6a082bcbf4c97f02da3b0e5d89b0d3
Eduardo Habkost <ehabkost@raisama.net>
2009-07-26 12:03:00 -0300

    Send NICKSERV IDENTIFY command after connecting
    Signed-off-by: Eduardo Habkost <ehabkost@raisama.net>

--- bot_sql.py
+++ bot_sql.py
@@ -156,8 +156,10 @@ sock.send('USER %s \'\' \'\' :%s\r\n' % (nick, 'python'))
 
 # regain nick, if it is in use
 sock.send('NICKSERV REGAIN %s %s\r\n' % (nick, password))
+
 # change to the real nick
 sock.send('NICK %s \r\n' % nick)
+sock.send('NICKSERV IDENTIFY %s\r\n' % (password))
 
 # join the channel
 sock.send('JOIN %s \r\n' % channel)
